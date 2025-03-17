66f11f3eea308ce6850c48b17b4eb6d83b683e4d
Alvaro R Martin <83295669+exagonsoft@users.noreply.github.com>
2025-03-17 17:25:31 +0100

    #2 Update Auth View

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -65,145 +65,162 @@ class _AuthScreenState extends State<AuthScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double padding = screenWidth * 0.07; // Responsive horizontal padding
+
     return Scaffold(
       backgroundColor: Colors.pink.shade50,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Title Text
-              Text(
-                isLogin ? "Welcome Back!" : "Create Account",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.pink.shade700,
-                ),
-              ),
-              SizedBox(height: 10),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: padding),
+            child: Column(
+              children: [
+                SizedBox(height: screenHeight * 0.2), // Dynamic top spacing
 
-              // Subtitle
-              Text(
-                "Join us for the best beauty experience!",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
-              ),
-              SizedBox(height: 30),
-
-              // Email Input
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  prefixIcon: Icon(Icons.email, color: Colors.pink.shade400),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-
-              // Password Input
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.lock, color: Colors.pink.shade400),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Login/Register Button
-              ElevatedButton(
-                onPressed: _authenticate,
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  backgroundColor: Colors.pink.shade600,
-                  elevation: 5,
-                ),
-                child: Text(
-                  isLogin ? "Login" : "Register",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(height: 10),
-
-              // Toggle between Login/Register
-              TextButton(
-                onPressed: () {
-                  setState(() => isLogin = !isLogin);
-                },
-                child: Text(
-                  isLogin
-                      ? "Create an account"
-                      : "Already have an account? Login",
+                // Title Text
+                Text(
+                  isLogin ? "Bienvenido!" : "Crear Cuenta",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.pink.shade700,
+                    fontSize: screenWidth * 0.07, // Responsive font size
                     fontWeight: FontWeight.bold,
+                    color: Colors.pink.shade700,
                   ),
                 ),
-              ),
+                SizedBox(height: screenHeight * 0.015),
 
-              // Divider
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child:
-                            Divider(thickness: 1, color: Colors.grey.shade400)),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text("OR",
-                          style: TextStyle(color: Colors.grey.shade600)),
+                // Subtitle
+                Text(
+                  "¡Únete a nosotras para la mejor experiencia de belleza!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.04, // Responsive font size
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.04),
+
+                // Email Input
+                _buildTextField("Email", Icons.email, _emailController),
+                SizedBox(height: screenHeight * 0.02),
+
+                // Password Input
+                _buildTextField("Contraseña", Icons.lock, _passwordController,
+                    obscureText: true),
+                SizedBox(height: screenHeight * 0.03),
+
+                // Login/Register Button
+                _buildMainButton(isLogin ? "Acceso" : "Registro", _authenticate),
+                SizedBox(height: screenHeight * 0.01),
+
+                // Toggle between Login/Register
+                TextButton(
+                  onPressed: () {
+                    setState(() => isLogin = !isLogin);
+                  },
+                  child: Text(
+                    isLogin
+                        ? "Crear Cuenta"
+                        : "¿Ya tienes una cuenta? Iniciar sesión",
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.04, // Responsive font size
+                      color: Colors.pink.shade700,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                        child:
-                            Divider(thickness: 1, color: Colors.grey.shade400)),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-
-              // Google Sign-In Button
-              ElevatedButton.icon(
-                onPressed: _signInWithGoogle,
-                icon: Icon(Icons.login, color: Colors.white),
-                label: Text(
-                  "Sign in with Google",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
                   ),
-                  backgroundColor: Colors.red.shade600,
-                  elevation: 5,
                 ),
-              ),
-            ],
+
+                // Divider
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                              thickness: 1, color: Colors.grey.shade400)),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text("OR",
+                            style: TextStyle(color: Colors.grey.shade600)),
+                      ),
+                      Expanded(
+                          child: Divider(
+                              thickness: 1, color: Colors.grey.shade400)),
+                    ],
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.015),
+
+                // Google Sign-In Button
+                _buildMainButton("Iniciar sesión con Google", _signInWithGoogle,
+                    color: Colors.red.shade600),
+                SizedBox(height: screenHeight * 0.05),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildTextField(
+      String label, IconData icon, TextEditingController controller,
+      {bool obscureText = false}) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon, color: Colors.pink.shade400),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainButton(String text, VoidCallback onPressed,
+      {IconData? icon, Color? color, double? width}) {
+    return icon != null
+        ? ElevatedButton.icon(
+            onPressed: onPressed,
+            icon: Icon(icon, color: Colors.white),
+            label: Text(
+              text,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            style: ElevatedButton.styleFrom(
+              padding:
+                  EdgeInsets.symmetric(vertical: 14, horizontal: width ?? 20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              backgroundColor: color ?? Colors.pink.shade600,
+              elevation: 5,
+            ),
+          )
+        : ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              padding:
+                  EdgeInsets.symmetric(vertical: 14, horizontal: width ?? 20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              backgroundColor: color ?? Colors.pink.shade600,
+              elevation: 5,
+            ),
+            child: Center(
+              // Ensures label is always centered
+              child: Text(
+                text,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
   }
 }
