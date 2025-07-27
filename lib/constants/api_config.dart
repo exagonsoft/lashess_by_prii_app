0236ca1d34d1c4c1bf5386a2e09f7f23b50f5438
Alvaro R Martin <83295669+exagonsoft@users.noreply.github.com>
2025-07-27 22:31:15 +0200

    #34 Business Info Screen Implementation

--- lib/constants/api_config.dart
+++ lib/constants/api_config.dart
@@ -1,5 +1,54 @@
+import 'package:lashess_by_prii_app/interfaces/interfaces.dart';
+
 const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
 final String baseApi = isProduction
     ? 'https://lashees_by_prii.com/api'
     : 'http://192.168.1.15:3000/api';
+
+final services = [
+      ServiceInfo(
+        name: "Extensiones Clásicas",
+        price: "U\$S 30",
+        description: "Técnica pelo a pelo para un look natural y definido.",
+        imagePaths: [
+          'assets/images/classic1.jpg',
+        ],
+      ),
+      ServiceInfo(
+        name: "Volumen Ruso",
+        price: "U\$S 45",
+        description:
+            "Ventiladores de 2 a 6 pestañas ultra finas para mayor densidad y volumen dramático.",
+        imagePaths: [
+          'assets/images/russian1.jpg',
+        ],
+      ),
+      ServiceInfo(
+        name: "Retoque (15 días)",
+        price: "U\$S 20",
+        description:
+            "Reaplicación parcial para mantener la frescura y densidad de tu diseño inicial.",
+        imagePaths: [
+          'assets/images/retouch1.jpg',
+        ],
+      ),
+      ServiceInfo(
+        name: "Laminado de Pestañas",
+        price: "U\$S 25",
+        description:
+            "Tratamiento que eleva y da forma natural a tus pestañas, ideal para un look sin extensiones.",
+        imagePaths: [
+          'assets/images/laminado1.jpg',
+        ],
+      ),
+      ServiceInfo(
+        name: "Lifting + Tinte",
+        price: "U\$S 35",
+        description:
+            "Combinación de elevación con color para realzar tus pestañas naturales con intensidad.",
+        imagePaths: [
+          'assets/images/lifting1.jpg',
+        ],
+      ),
+    ];
---
+++ lib/interfaces/interfaces.dart
@@ -0,0 +1,13 @@
+class ServiceInfo {
+  final String name;
+  final String price;
+  final String description;
+  final List<String> imagePaths;
+
+  ServiceInfo({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imagePaths,
+  });
+}
